62f22c8f23a9d70f2003289ad112669f98bced34
02YM <dydals4289@gmail.com>
2025-05-10 18:09:29 +0900

    Mathf_Start
    Mathf_Start

---
+++ Assets/00.Mathf/Unity_Engine_Mathf.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Unity_Engine_Mathf : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
